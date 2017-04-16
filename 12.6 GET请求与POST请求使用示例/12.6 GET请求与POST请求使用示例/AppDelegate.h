c59db7e2ae13268cd9892598fc1c98e21bcbb846
test <422538722@qq.com>
2017-04-16 16:18:33 +0800

    12.6
    GET请求与POST请求使用示例

---
+++ 12.6 GET请求与POST请求使用示例/12.6 GET请求与POST请求使用示例/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  12.6 GET请求与POST请求使用示例
+//
+//  Created by 李维佳 on 2017/4/4.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 12.6 GET请求与POST请求使用示例/12.6 GET请求与POST请求使用示例/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  12.6 GET请求与POST请求使用示例
+//
+//  Created by 李维佳 on 2017/4/4.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
